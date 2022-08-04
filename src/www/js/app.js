e1993d848fe9ab3323a1b1067bdfb9c0f9e3b07f
Dartegnian <root@dartegnian.com>
2022-08-05 01:54:07 +0800

    Retool version numbers to match source

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -270,7 +270,7 @@ new Vue({
 
     Promise.resolve().then(async () => {
       const currentRelease = await this.api.getRelease();
-      const latestRelease = await fetch('https://dartegnian.github.io/wg-easy-m3/changelog.json')
+      const latestRelease = await fetch('https://weejewel.github.io/wg-easy/changelog.json')
         .then(res => res.json())
         .then(releases => {
           const releasesArray = Object.entries(releases).map(([version, changelog]) => ({
